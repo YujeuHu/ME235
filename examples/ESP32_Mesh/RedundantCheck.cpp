cc3e660cd34d6b4e70c17b810717e8f95da2d34c
Kaw0ru <tomzhang1995@gmail.com>
2018-04-04 10:45:08 -0700

    combine two methods...

--- examples/ESP32_Mesh/RedundantCheck.cpp
+++ examples/ESP32_Mesh/RedundantCheck.cpp
@@ -1,56 +1,59 @@
 #include "RedundantCheck.h"
 
 RedundantChecker::RedundantChecker() {
-    history[0] = 0;
+    history[0][0] = 0;
     numOfRecord = 0;
 }
 
 bool RedundantChecker::reset() {
-    int cunt = sizeof(history) / sizeof(history[0]);
+    int cunt = sizeof(history) / (sizeof(history[0][0]) * 2);
     for (int i = 0; i < cunt; i++){
-        history[i] = 0;
+        history[i][0] = 0;
     }    
     numOfRecord = 0;
-    return history[0] == 0;
+    return history[0][0] == 0;
 }
 
 bool RedundantChecker::check(String msg) {
     DynamicJsonBuffer restoreJsonBuffer;
     JsonObject& JSONRestored = restoreJsonBuffer.parseObject(msg);
+    int lengthOfMsg = msg.length();
 
-    if (history[0] == 0) { //First call
-        history[0] = JSONRestored["DeviceID"];
+    if (history[0][0] == 0) { //First call
+        history[0][0] = JSONRestored["DeviceID"];
+        history[0][1] = lengthOfMsg;
         //Serial.println("First Call");
         return false;
     } else { // History contains sth
-        int cunt = sizeof(history) / sizeof(history[0]);
+        int cunt = sizeof(history) / (sizeof(history[0][0]) * 2);
         for (int i = 0; i < cunt; i++) { // treaverse the entire array to find same ID
-            if (history[i] == JSONRestored["DeviceID"]) { // Redundant ID found
+            if (history[i][0] == JSONRestored["DeviceID"] & history[i][1] == lengthOfMsg;) { // Redundant ID found
                 return true;
             }
         }
         numOfRecord ++; // No Redundancy Found
-        history[numOfRecord] = JSONRestored["DeviceID"];
+        history[numOfRecord][0] = JSONRestored["DeviceID"];
+        history[numOfRecord][1] == lengthOfMsg;
         return false;
     }
 
-bool RedundantChecker::check(String msg, unit16_t &lengthOfMsg) {
-    DynamicJsonBuffer restoreJsonBuffer;
-    JsonObject& JSONRestored = restoreJsonBuffer.parseObject(msg);
-    lengthOfMsg = msg.length();
-    if (history[0] == 0) { //First call
-        history[0] = JSONRestored["DeviceID"];
-        //Serial.println("First Call");
-        return false;
-    } else { // History contains sth
-        int cunt = sizeof(history) / sizeof(history[0]);
-        for (int i = 0; i < cunt; i++) { // treaverse the entire array to find same ID
-            if (history[i] == JSONRestored["DeviceID"]) { // Redundant ID found
-                return true;
-            }
-        }
-        numOfRecord ++; // No Redundancy Found
-        history[numOfRecord] = JSONRestored["DeviceID"];
-        return false;
-    }
-}
+// bool RedundantChecker::check(String msg, unit16_t &lengthOfMsg, ) {
+//     DynamicJsonBuffer restoreJsonBuffer;
+//     JsonObject& JSONRestored = restoreJsonBuffer.parseObject(msg);
+//     lengthOfMsg = msg.length();
+//     if (history[0] == 0) { //First call
+//         history[0] = JSONRestored["DeviceID"];
+//         //Serial.println("First Call");
+//         return false;
+//     } else { // History contains sth
+//         int cunt = sizeof(history) / sizeof(history[0]);
+//         for (int i = 0; i < cunt; i++) { // treaverse the entire array to find same ID
+//             if (history[i] == JSONRestored["DeviceID"]) { // Redundant ID found
+//                 return true;
+//             }
+//         }
+//         numOfRecord ++; // No Redundancy Found
+//         history[numOfRecord] = JSONRestored["DeviceID"];
+//         return false;
+//     }
+// }
--- examples/ESP32_Mesh/RedundantCheck.h
+++ examples/ESP32_Mesh/RedundantCheck.h
@@ -1,14 +1,14 @@
 #include <ArduinoJson.h>
 
-
+#define MAX_NUM_OF_NODE 5
 class RedundantChecker {
     public:
     RedundantChecker();
     bool check(String msg);
-    bool check(String msg, unit16_t &lengthOfMsg);
+    // bool check(String msg, unit16_t &lengthOfMsg);
     bool reset();
 
     private:
-    int history[5];
+    int history[MAX_NUM_OF_NODE][2];
     int numOfRecord;
 };
