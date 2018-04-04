53931f0654add193dce2f531d9e52793b5789884
Kaw0ru <tomzhang1995@gmail.com>
2018-04-04 10:36:12 -0700

    House Keeping...

--- redundantCheck/RedundantCheck.h
+++ redundantCheck/RedundantCheck.h
@@ -5,8 +5,10 @@ class RedundantChecker {
     public:
     RedundantChecker();
     bool check(String msg);
+    bool check(String msg, unit16_t &lengthOfMsg);
     bool reset();
 
     private:
     int history[5];
+    int numOfRecord;
 };
